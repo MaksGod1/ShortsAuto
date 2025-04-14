9db48d0a1b25376488f66a647ecc7cf08417a797
MaksGod1 <mgord516@gmail.com>
2025-04-14 16:06:17 +0200

    Update shorts.py
    delete unused library

--- shorts.py
+++ shorts.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.common.exceptions import NoSuchElementException
 import time
